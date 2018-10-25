1d3374b480e50403fe62fc2ee194aedee6d7a256
masaki345 <furan345@gmail.com>
2018-10-25 05:03:58 +0000

    create add flash

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,14 +5,19 @@ class ArticlesController < ApplicationController
   end
   
   def create
-    #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-#    render plain: @article.errors.full_messages.inspect
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "successful created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+    @article = Article.find(params[:id])
   end
 
-  
   private
     def article_params
       params.require(:article).permit(:title,:description)
